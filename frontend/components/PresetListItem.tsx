8806bec5434935a1d514d2966984c75f8f2b2ce9
Saulo Costa <saulotarsobc@gmail.com>
2023-12-25 11:18:37 -0300

    :fire:

--- frontend/components/PresetListItem.tsx
+++ frontend/components/PresetListItem.tsx
@@ -4,28 +4,46 @@ import {
   IconEdit,
   IconTrashXFilled,
 } from "@tabler/icons-react";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 type PresetListItemProps = {
   presetId: number;
 };
 
 export default function PresetListItem({ presetId }: PresetListItemProps) {
-  const handleEdit = useCallback((presetId: number) => {
-    console.log(presetId);
-  }, []);
+  const [channel, setChannel] = useState("8");
 
-  const handleDelete = useCallback((presetId: number) => {
-    console.log(presetId);
-  }, []);
+  const handleEdit = useCallback((presetId: number) => {}, []);
 
-  const handleSave = useCallback((presetId: number) => {
-    console.log(presetId);
-  }, []);
+  const handleDelete = useCallback((presetId: number) => {}, []);
 
-  const handleCall = useCallback((presetId: number) => {
-    console.log(presetId);
-  }, []);
+  const handleSave = useCallback(
+    // Name  Type  R/O  Description  Example
+    // channel  int  R  The PTZ channel index; starting from 1  1
+    // arg1  int  R  The preset number; starting from 1  2
+    // arg2  char[256]  R  The preset name  "preset2"
+    (presetId: number) => {
+      fetch(
+        "http://127.0.0.1:3000/?action=start&code=SetPreset&channel=1&arg1=0&arg2=2&arg3=0"
+      )
+        .then((data) => console.log(data.body))
+        .catch((e: Error) => console.warn(e.message));
+    },
+    [channel]
+  );
+
+  const handleCall = useCallback(
+    (presetId: number) => {
+      fetch(
+        `http://127.0.0.1:3000/?action=start&code=GotoPreset&channel=${channel}&arg2=${presetId}`
+      )
+        .then((data) => {
+          console.log(data.body);
+        })
+        .catch((e: Error) => console.warn(e.message));
+    },
+    [channel]
+  );
 
   return (
     <div className="preset w-[350px] bg-slate-600 flex flex-row justify-between p-1 mt-[1px] rounded-md">
@@ -44,8 +62,8 @@ export default function PresetListItem({ presetId }: PresetListItemProps) {
         value={"Nome " + presetId}
       />
 
-      <div className="flex">
-        <IconEdit onClick={() => handleEdit(presetId)} />
+      <div className="flex gap-1">
+        {/* <IconEdit onClick={() => handleEdit(presetId)} /> */}
         <IconTrashXFilled onClick={() => handleDelete(presetId)} />
         <IconDeviceFloppy onClick={() => handleSave(presetId)} />
         <IconCircleCheckFilled onClick={() => handleCall(presetId)} />
