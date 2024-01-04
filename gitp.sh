23bb735c6d094986a24fa1e48ac11e402231c29b
Saulo Costa <saulotarsobc@gmail.com>
2024-01-04 13:18:33 -0300

    @saulotarsobc :fire:

--- gitp.sh
+++ gitp.sh
@@ -9,7 +9,7 @@ echo -e "\033[33m\n\n=====> git add --all \n\033[0m"
 git add --all;
 
 echo -e "\033[33m\n\n=====> git commit -m ':fire:$1' \n\033[0m"
-git commit -m ":fire:$1";
+git commit -m "@saulotarsobc :fire:$1";
 
 echo -e "\033[33m\n\n=====> git push \n\033[0m"
 git push;
